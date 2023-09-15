186880e91e57bd50dee10034da0fafee39a5ddbf
Mathias Boeck <Mathias.Boeck@dlr.de>
2023-09-15 18:22:53 +0200

    feat: add layer from file

---
+++ src/app/import-data/import-data.component.ts
@@ -0,0 +1,43 @@
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import olMap from 'ol/Map';
+import { addFileLayer } from '../map.utills';
+
+@Component({
+  selector: 'app-import-data',
+  templateUrl: './import-data.component.html',
+  styleUrls: ['./import-data.component.scss']
+})
+export class ImportDataComponent {
+  @ViewChild('fileInput') fileInput: ElementRef;
+  @Input() map!: olMap;
+  reader = new FileReader();
+  selectedFile: any;
+  constructor() {
+
+  }
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0] ?? null;
+    if (this.selectedFile) {
+
+      this.reader.readAsText(this.selectedFile, "UTF-8");
+      this.reader.onload = (evt) => {
+        const fileData = evt.target.result;
+        if (typeof fileData !== 'string') {
+          addFileLayer(this.map, fileData.toString());
+        } else {
+          addFileLayer(this.map, fileData);
+        }
+      }
+
+    }
+
+  }
+
+  removeFileLayers() {
+    addFileLayer(this.map, null, true);
+    this.fileInput.nativeElement.value = '';
+    this.selectedFile = null;
+  }
+
+}
--- src/app/map.utills.ts
+++ src/app/map.utills.ts
@@ -5,9 +5,11 @@ import { get as getProjection } from 'ol/proj';
 import { Layer, LayerEntity } from './ogc.types';
 
 import { transform } from 'ol/proj.js';
+import { GPX, GeoJSON } from 'ol/format';
 import Map from 'ol/Map';
 import VectorLayer from 'ol/layer/Vector';
 import PointGeom from 'ol/geom/Point';
+import LineGeom from 'ol/geom/LineString';
 import VectorSource from 'ol/source/Vector';
 import { Coordinate } from 'ol/coordinate';
 import { Feature } from 'ol';
@@ -69,22 +71,100 @@ export function addLocationLayer(map: Map, coordinates: Coordinate) {
       ]
     }),
     style: () => {
-      return new Style({
-        image: new CircleStyle({
-          radius: 6,
-          stroke: new Stroke({
-            color: 'black',
-            width: 1,
-          }),
-          fill: new Fill({
-            color: 'rgba(235, 156, 70, 0.8)',
+      return [
+        new Style({
+          image: new CircleStyle({
+            radius: 10,
+            fill: new Fill({
+              // color: 'rgba(0, 77, 64, 0.8)
+              color: 'rgba(256, 256, 256, 0.3)'
+            })
+          })
+        }),
+        new Style({
+          image: new CircleStyle({
+            radius: 7,
+            /* stroke: new Stroke({
+              color: 'rgba(256, 256, 256, 0.8)',
+              width: 2,
+            }), */
+            fill: new Fill({
+              //color: 'rgba(235, 156, 70, 0.8)'
+              color: 'rgba(0, 0, 0, 0.5)'
+            })
           })
         })
-      })
+      ]
     }
   })
   map.addLayer(locationPont);
   return locationPont;
+}
+
+/**
+ * 
+ * @param data GPX or GeoJSON
+ */
+export function addFileLayer(map: Map, data: string, remove?: boolean) {
+  const mapLayers = map.getAllLayers().filter(l => l.get('id')?.includes('FileLayer'));
+
+  if (remove) {
+    mapLayers.forEach(l => {
+      map.removeLayer(l);
+    })
+
+    return;
+  }
+
+  let features = [];
+
+  const isGpx = data.indexOf('<?xml') === 0;
+  const mapEpsg = map.getView().getProjection().getCode();
+  if (isGpx) {
+    const gpx = new GPX({
+    });
+    features = gpx.readFeatures(data).map(f => {
+      f.getGeometry().transform('EPSG:4326', mapEpsg)
+      return f;
+    });
+  } else {
+    const isJson = JSON.parse(data);
+    if (isJson) {
+      const geoJson = new GeoJSON({
+        dataProjection: 'EPSG:4326',
+        featureProjection: mapEpsg
+      });
+
+      if (isJson.type === 'FeatureCollection') {
+        features = geoJson.readFeatures(data);
+      } else if (isJson.type === 'Feature') {
+        features = [geoJson.readFeature(data)];
+      }
+    }
+  }
+
+  if (features.length) {
+    const fileLayer = new VectorLayer<VectorSource<PointGeom | LineGeom>>({
+      source: new VectorSource({
+        features: features
+      })
+    })
+    if (mapLayers.length) {
+      fileLayer.set('id', 'FileLayer');
+    } else {
+      fileLayer.set('id', `FileLayer-${mapLayers.length + 1}`);
+    }
+
+    const layerExt = fileLayer.getSource().getExtent();
+    map.getView().fit(layerExt);
+
+
+
+    map.addLayer(fileLayer);
+  }
+
+
+
 }
 
 
